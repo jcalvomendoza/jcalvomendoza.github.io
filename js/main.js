aac2a0de46dd4b26b32763a843693f99e2b53df2
jcalvomendoza <30542007+jcalvomendoza@users.noreply.github.com>
2017-09-01 16:00:23 -0500

    website redesign
    Changed html, css, javascript files from scratch + images.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,11 @@
  * Article URL: http://ihatetomatoes.net/simple-parallax-scrolling-tutorial/
  */
 
+$(window).load(function() {
+	$("body").removeClass("loading");
+  });
+
+
 ( function( $ ) {
 	
 	// Setup variables
@@ -16,6 +21,7 @@
 	$subSlide = $('.homeSlideSub');
 	$footer = $('.footer');
 	$body = $('body');
+	
 	
     //FadeIn all sections   
 	$body.imagesLoaded( function() {
@@ -43,11 +49,6 @@
 			winH = 550;
 		} 
 	    
-	    // Resize our slides
-		$slide.height(winH);
-		$subSlide.height(winH/4);
-		$footer.height(winH/20);
-	    
 	    // Refresh Skrollr after resizing our sections
 	    
 	    
@@ -64,6 +65,90 @@ $(document).on("scroll", function() {
 		$("header").removeClass("expandedNavigationBar").addClass("collapsedNavigationBar");
 	}
 	
+});
+
+/* This nasty block was my first try to get the fade on scroll effect, so clueless
+
+$(document).on("scroll", function() {
+        if($(document).scrollTop()> $(window).height()*.5 && $(document).scrollTop()< $(window).height()*.55) {
+            $(".about_me_blurb").css("opacity", ".1")
+		} else if ($(document).scrollTop()> $(window).height()*.55 && $(document).scrollTop()< $(window).height()*.6) {
+			$(".about_me_blurb").css("opacity", ".2")
+		} else if ($(document).scrollTop()> $(window).height()*.6 && $(document).scrollTop()< $(window).height()*.65) {
+			$(".about_me_blurb").css("opacity", ".3")
+		} else if ($(document).scrollTop()> $(window).height()*.65 && $(document).scrollTop()< $(window).height()*.7) {
+			$(".about_me_blurb").css("opacity", ".4")
+		} else if ($(document).scrollTop()> $(window).height()*.7 && $(document).scrollTop()< $(window).height()*.75) {
+			$(".about_me_blurb").css("opacity", ".5")
+		} else if ($(document).scrollTop()> $(window).height()*.75 && $(document).scrollTop()< $(window).height()*.8) {
+			$(".about_me_blurb").css("opacity", ".6")
+		} else if ($(document).scrollTop()> $(window).height()*.8 && $(document).scrollTop()< $(window).height()*.85) {
+			$(".about_me_blurb").css("opacity", ".7")
+		} else if ($(document).scrollTop()> $(window).height()*.85 && $(document).scrollTop()< $(window).height()*.9) {
+			$(".about_me_blurb").css("opacity", ".8")
+		} else if ($(document).scrollTop()> $(window).height()*.9 && $(document).scrollTop()< $(window).height()*.95) {
+			$(".about_me_blurb").css("opacity", ".9")
+		} else if ($(document).scrollTop()> $(window).height()*.95) {
+			$(".about_me_blurb").css("opacity", "1")
+		} else {
+            $(".about_me_blurb").css("opacity", "0");
+        }
+        
+    });*/
+
+	/* Then I figured this badboy out */
+	
+	$(document).on("scroll", function() {
+		var scrollpct = $(document).scrollTop()/$(window).height();
+		$(".about_me_blurb").css("opacity", scrollpct.toString());
+	});
+
+	$(document).on("scroll", function() {
+		var scrollpct = ($(document).scrollTop()/($(window).height()*3.5))/5;
+		$(".graduation_pic").css("opacity", scrollpct.toString());
+	});
+
+
+/* NAV BAR ANIMATIONS */
+
+/* ABOUT ME NAV */
+
+	$(document).on("scroll", function() {
+		if (($(document).scrollTop()/($(window).height())) > .9 && ($(document).scrollTop()/($(window).height())) < 1.9) {
+			$(".about_me_nav_link").css("opacity", "1");
+		} else {$(".about_me_nav_link").css("opacity", ".2");
+	}
+	});
+
+	$(".about_me_nav_link").on("mouseover", function() {
+		$(".about_me_nav_link").css("opacity", "1");
+	});
+	$(".about_me_nav_link").on("mouseout", function() {
+		if(($(document).scrollTop()/($(window).height())) > .9 && ($(document).scrollTop()/($(window).height())) < 1.9){
+			$(".about_me_nav_link").css("opacity", "1");
+		} else {
+		$(".about_me_nav_link").css("opacity", ".2");
+		}
+	});
+
+/* EDUCATION NAV */
+
+$(document).on("scroll", function() {
+	if (($(document).scrollTop()/($(window).height())) > 1.9 && ($(document).scrollTop()/($(window).height())) < 2.9) {
+		$(".education_nav_link").css("opacity", "1");
+	} else {$(".education_nav_link").css("opacity", ".2");
+}
+});
+
+$(".education_nav_link").on("mouseover", function() {
+	$(".education_nav_link").css("opacity", "1");
+});
+$(".education_nav_link").on("mouseout", function() {
+	if(($(document).scrollTop()/($(window).height())) > 1.9 && ($(document).scrollTop()/($(window).height())) < 2.9){
+		$(".education_nav_link").css("opacity", "1");
+	} else {
+	$(".education_nav_link").css("opacity", ".2");
+	}
 });
 
 
